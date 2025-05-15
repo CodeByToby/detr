fcbcb692f02a69a0fca91edfca6cfd28f1fb47e8
CodeByToby <163883132+CodeByToby@users.noreply.github.com>
2025-05-15 23:37:58 +0200

    `datasets.transforms` has no method `ColorJitter`, Comment for now

--- datasets/custom.py
+++ datasets/custom.py
@@ -30,7 +30,7 @@ def make_custom_transforms(image_set):
                     T.RandomResize(scales, max_size=1333),
                 ])
             ),
-            T.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.05),  # Adds visual diversity
+            #T.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.05),  # Adds visual diversity
             normalize,
         ])
 
