115060004c0b00f97b756bac46c82ba7874c7623
CodeByToby <163883132+CodeByToby@users.noreply.github.com>
2025-05-15 23:24:58 +0200

    Add custom transforms

--- datasets/custom.py
+++ datasets/custom.py
@@ -7,6 +7,39 @@ Modified from the coco.py file and with help from https://gist.github.com/woctez
 from pathlib import Path
 from .coco import CocoDetection, make_coco_transforms
 
+def make_custom_transforms(image_set):
+# Taken from the coco.py file
+
+    normalize = T.Compose([
+        T.ToTensor(),
+        T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    scales = [480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800]
+
+    if image_set == 'train':
+        return T.Compose([
+            T.RandomHorizontalFlip(),
+            T.RandomSelect(
+                T.RandomResize(scales, max_size=1333),
+                T.Compose([
+                    T.RandomResize([300, 450, 600]), # More aggressive zoom
+                    T.RandomSizeCrop(384, 600),
+                    T.RandomResize(scales, max_size=1333),
+                ])
+            ),
+            T.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.05),  # Adds visual diversity
+            normalize,
+        ])
+
+    if image_set == 'val':
+        return T.Compose([
+            T.RandomResize([800], max_size=1333),
+            normalize,
+        ])
+
+    raise ValueError(f'unknown {image_set}')
+
 def build(image_set, args):
     root = Path(args.coco_path)
     assert root.exists(), f'provided path {root} does not exist'
@@ -17,5 +50,5 @@ def build(image_set, args):
     }
 
     img_folder, ann_file = PATHS[image_set]
-    dataset = CocoDetection(img_folder, ann_file, transforms=make_coco_transforms(image_set), return_masks=args.masks)
+    dataset = CocoDetection(img_folder, ann_file, transforms=make_custom_transforms(image_set), return_masks=args.masks)
     return dataset
