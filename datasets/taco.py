058f4383e9268c78ee9635a648f9f0c8fe39f0d2
CodeByToby <163883132+CodeByToby@users.noreply.github.com>
2025-05-29 14:35:25 +0200

    Modify custom TACO transforms

--- datasets/taco.py
+++ datasets/taco.py
@@ -6,6 +6,37 @@ Modified from the coco.py file and with help from https://gist.github.com/woctez
 
 from pathlib import Path
 from .coco import CocoDetection, make_coco_transforms
+import datasets.transforms as T
+
+def make_taco_transforms(image_set):
+    normalize = T.Compose([
+        T.ToTensor(),
+        T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    scales = [384, 416, 448, 480, 512, 544, 576, 608, 640] # Modified
+
+    if image_set == 'train':
+        return T.Compose([
+            T.RandomHorizontalFlip(),
+            T.RandomSelect(
+                T.RandomResize(scales, max_size=1333),
+                T.Compose([
+                    T.RandomResize([384, 477, 600]), # Modified from [400, 500, 600]
+                    T.RandomSizeCrop(384, 600),
+                    T.RandomResize(scales, max_size=1333),
+                ])
+            ),
+            normalize,
+        ])
+
+    if image_set == 'val':
+        return T.Compose([
+            T.RandomResize([800], max_size=1333),
+            normalize,
+        ])
+
+    raise ValueError(f'unknown {image_set}')
 
 def build(image_set, args):
     root = Path(args.coco_path)
